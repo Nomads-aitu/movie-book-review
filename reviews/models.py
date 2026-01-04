d7bba9381412a9b92dbfaeefdbf879d58bcbad7c
alaskanotavalible <aliaskarmnv2@gmail.com>
2026-01-04 20:08:25 +0500

    templates

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
