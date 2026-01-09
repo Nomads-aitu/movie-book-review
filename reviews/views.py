182c2794524adc5e07c812c417bb1b84425ded3e
kazhymukanboris-dev <kazhymukanboris@gmail.com>
2026-01-09 17:06:02 +0500

    Update views.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .models import MediaItem
 
 def home(request):
     return render(request, 'reviews/home.html')
+
+def movie_list(request):
+    # This grabs movies AND their linked reviews efficiently
+    movies = MediaItem.objects.filter(category='Movie').prefetch_related('reviews')
+    return render(request, 'reviews/list.html', {'items': movies, 'title': 'Movies'})
+
+def book_list(request):
+    # This grabs books AND their linked reviews efficiently
+    books = MediaItem.objects.filter(category='Book').prefetch_related('reviews')
+    return render(request, 'reviews/list.html', {'items': books, 'title': 'Books'})
