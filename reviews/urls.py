a09f433f033c6adf4583156787f12007ba6e85eb
kazhymukanboris-dev <kazhymukanboris@gmail.com>
2026-01-09 17:05:45 +0500

    Update urls.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('movies/', views.movie_list, name='movie_list'),
+    path('books/', views.book_list, name='book_list'),
 ]
